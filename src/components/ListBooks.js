fd9f8a65a33994dbb6f6fd77618767b60f16f709
Anders Quist <anders@qinfo.se>
2018-11-09 08:46:13 +0100

    refactor: made ListBooks into a function component.

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,56 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import BookShelf from "./BookShelf";
 import * as Const from '../constants';
 
-class ListBooks extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array,
-    onShelfChange: PropTypes.func.isRequired
-  };
-
-  render() {
-    const { title, books, onShelfChange } = this.props;
-    const currentlyReading = books
-      .filter( ({ shelf }) => shelf === Const.CURRENTLY_READING);
-    const wantToRead = books
-      .filter( ({ shelf }) => shelf === Const.WANT_TO_READ);
-    const read = books
-      .filter( ({ shelf }) => shelf === Const.READ);
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>{ title }</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <BookShelf
-              title="Currently Reading"
-              books={currentlyReading}
-              onShelfChange={onShelfChange}
-            />
-            <BookShelf
-              title="Want to Read"
-              books={wantToRead}
-              onShelfChange={onShelfChange}
-            />
-            <BookShelf
-              title="Read"
-              books={read}
-              onShelfChange={onShelfChange}
-            />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to='/search'>
-            Add a book
-          </Link>
+const ListBooks = ({ title, books, onShelfChange }) => {
+  const currentlyReading = books
+    .filter( ({ shelf }) => shelf === Const.CURRENTLY_READING);
+  const wantToRead = books
+    .filter( ({ shelf }) => shelf === Const.WANT_TO_READ);
+  const read = books
+    .filter( ({ shelf }) => shelf === Const.READ);
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>{ title }</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <BookShelf
+            title="Currently Reading"
+            books={currentlyReading}
+            onShelfChange={onShelfChange}
+          />
+          <BookShelf
+            title="Want to Read"
+            books={wantToRead}
+            onShelfChange={onShelfChange}
+          />
+          <BookShelf
+            title="Read"
+            books={read}
+            onShelfChange={onShelfChange}
+          />
         </div>
       </div>
-    )
-  }
-}
+      <div className="open-search">
+        <Link to='/search'>
+          Add a book
+        </Link>
+      </div>
+    </div>
+  )
+};
+
+ListBooks.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array,
+  onShelfChange: PropTypes.func.isRequired
+};
 
 export default ListBooks;
